2e4baa22f13dad43f5f336dca2ffee9a3e5deeda
Ayan Manna <ayanmanna858@gmail.com>
2025-09-18 13:36:37 +0530

    update

--- Backend/routes/location.route.js
+++ Backend/routes/location.route.js
@@ -1,20 +1,25 @@
 import express from "express";
- 
-import { createBusId, getAllBus, getAllBusDetails, getBusByDeviceId, getBusesAlongRoute, getLocation, updateBusLocation, updatelocation } from "../controllers/Location.controller.js";
- 
- 
+
+import {
+  createBusId,
+  getAllBus,
+  getAllBusDetails,
+  getBusByDeviceId,
+  getBusesAlongRoute,
+  getLocation,
+  updatelocation,
+} from "../controllers/Location.controller.js";
+
 const locationRoute = express.Router();
 
-locationRoute.put("/update/location",updatelocation)
-locationRoute.post("/create/newBus",createBusId)
-locationRoute.get("/get/location/:deviceID",getLocation)
-locationRoute.get('/get/search', getAllBus);
-locationRoute.get('/route/search', getBusesAlongRoute);
-locationRoute.get('/bus/:deviceId', getBusByDeviceId);
- 
- 
-locationRoute.post('/bus/update', updateBusLocation);
-locationRoute.get("/AllLocation",getAllBusDetails)
- 
+locationRoute.put("/update/location", updatelocation);
+locationRoute.post("/create/newBus", createBusId);
+locationRoute.get("/get/location/:deviceID", getLocation);
+locationRoute.get("/get/search", getAllBus);
+locationRoute.get("/route/search", getBusesAlongRoute);
+locationRoute.get("/bus/:deviceId", getBusByDeviceId);
+
+// locationRoute.post('/bus/update', updateBusLocation);
+locationRoute.get("/AllLocation", getAllBusDetails);
 
 export default locationRoute;
