6f47517c09a05c7f2d427b01d38f1c7e86285d04
santanu-atta03 <santanuatta87@gmail.com>
2025-09-20 17:56:52 +0530

    add to route

--- Backend/routes/journey.route.js
+++ Backend/routes/journey.route.js
@@ -1,6 +1,6 @@
-const express = require('express');
+import express from 'express'
 const router = express.Router();
-const journeyController = require('../controllers/Journey.controller.js');
+import journeyController from '../controllers/Journey.controller.js';
 const JourneyRoute = express.Router()
 // Journey planning endpoint
 JourneyRoute.post('/journey-planner', journeyController.planJourney);
