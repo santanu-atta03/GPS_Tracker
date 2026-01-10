d679cec7e46dbe1221f2bbc804974119937bcb3c
Ayan Manna <ayanmanna858@gmail.com>
2026-01-10 22:19:46 +0530

    fix: add missing semicolons in TurnstileCaptcha component

--- Frontend/src/components/shared/TurnstileCaptcha.jsx
+++ Frontend/src/components/shared/TurnstileCaptcha.jsx
@@ -2,13 +2,14 @@ import { useEffect, useRef } from "react";
 
 const TurnstileCaptcha = ({ onVerify }) => {
   const ref = useRef(null);
+  const hostname = window.location.hostname;
   let SITE_KEY = "";
   if (hostname === "localhost") {
-    SITE_KEY =  "0x4AAAAAACLpDPvIpoBRPbMe"
+    SITE_KEY = "0x4AAAAAACLpDPvIpoBRPbMe";
   } else if (hostname === "gps-map-nine.vercel.app") {
-    SITE_KEY =  "0x4AAAAAACLoq3Wk-omggzWD"
+    SITE_KEY = "0x4AAAAAACLoq3Wk-omggzWD";
   } else {
-    SITE_KEY =  "0x4AAAAAACLtFkqc1CCF60KP"
+    SITE_KEY = "0x4AAAAAACLtFkqc1CCF60KP";
   }
   useEffect(() => {
     if (!window.turnstile) return;
